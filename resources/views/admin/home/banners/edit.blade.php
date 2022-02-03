c634a449d2f28a4882341de97051e6973593c1b1
Osama Babiker <u.osamababiker@gmail.com>
2022-02-03 04:42:43 -0800

    normal commit

--- resources/views/admin/home/banners/edit.blade.php
+++ resources/views/admin/home/banners/edit.blade.php
@@ -86,5 +86,6 @@
                 @include('admin.layouts.feedback')
             @endif
             @include('admin.layouts.scripts')
+            <script src="{{ asset('js/add_banner.js') }}"></script>
             <!-- footer is here -->
             @include('admin.layouts.footer')
